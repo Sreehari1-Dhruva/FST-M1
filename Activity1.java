783d72c9aac06aed4332bc1abcd0f3a8f037b286
Sreehari1-Dhruva <Kristipati.Sreehari.Dhruva.Kumar@ibm.com>
2024-12-19 18:47:34 +0530

    Add files via upload

---
+++ Activity1.java
@@ -0,0 +1,15 @@
+package activities;
+
+public class Activity1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Car BMW = new Car("White", 2024, "Manual");
+		System.out.println();
+		BMW.displayCharacterstics();
+		BMW.accelerate();
+		BMW.brake();
+
+	}
+
+}
